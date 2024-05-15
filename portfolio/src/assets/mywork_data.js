8c45cc427c060f1819daf271ae5eaa4f9a41641f
Anthony Nguyen <anthonyng041@gmail.com>
2024-05-15 17:28:10 -0400

    Completed

--- portfolio/src/assets/mywork_data.js
+++ portfolio/src/assets/mywork_data.js
@@ -9,28 +9,28 @@ const mywork_data = [
         w_name: "E-commerce Web Application",
         w_img: project1_img,
         languages: ["HTML", "CSS", "Java", "SQL"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+        description: "Extended an e-commerce web application with new features and a redesigned the front-end."
     },
     {
         w_no: 2,
         w_name: "Database Application for Monitoring Employee Health",
         w_img: project2_img,
         languages: ["SQL", "PHP"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+        description: "Built an application database system that tracks employee health across different health facilities. Built a GUI."
     },
     {
         w_no: 3,
         w_name: "CNN for EEG Signal Decoding",
         w_img: project3_img,
         languages: ["Python", "NumPy", "PyTorch", "SpeechBrain-MOABB"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+        description: "Designed a convolutional neural network focusing on optimizing the architecture for EEG signal processing."
     },
     {
         w_no: 4,
         w_name: "CNN for Facial Emotion Classification",
         w_img: project4_img,
         languages: ["Python", "NumPy", "PyTorch", "Scikit-learn"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+        description: "Developed a convolutional neural network to classify the emotions of students."
     },
 ]
  
