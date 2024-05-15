3766443ca06e37eafd842cf2a8469913507354b8
Anthony Nguyen <anthonyng041@gmail.com>
2024-05-15 16:12:15 -0400

    Added New Features

--- portfolio/src/assets/mywork_data.js
+++ portfolio/src/assets/mywork_data.js
@@ -1,40 +1,36 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/ConcordiaCart.png'
+import project2_img from '../assets/HFESTS.png'
+import project3_img from '../assets/MachineLearning.png'
+import project4_img from '../assets/ArtificialIntelligence.png'
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_no: 1,
+        w_name: "E-commerce Web Application",
+        w_img: project1_img,
+        languages: ["HTML", "CSS", "Java", "SQL"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_no: 2,
+        w_name: "Database Application for Monitoring Employee Health",
+        w_img: project2_img,
+        languages: ["SQL", "PHP"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
     },
     {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_no: 3,
+        w_name: "CNN for EEG Signal Decoding",
+        w_img: project3_img,
+        languages: ["Python", "Numpy", "PyTorch", "SpeechBrain-MOABB"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
     },
     {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_no: 4,
+        w_name: "CNN for Facial Emotion Classification",
+        w_img: project4_img,
+        languages: ["Python", "Numpy", "PyTorch", "scikit-learn"],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
     },
 ]
  
--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./portfolio.css";
-import mywork_data from "../../assets/mywork_data.js"
+import mywork_data from "../../assets/mywork_data.js";
 
 const Portfolio = () => {
   return (
-    <div className='portfolio' id='portfolio'>
-        <div className="portfolio-title">
-            <h2>Portfolio</h2>
-        </div>
-        <div className="portfolio-container">
-            {mywork_data.map((work, index) =>{return <img key={index}src={work.w_img} alt=""/>})}
-        </div>
+    <div className="portfolio" id="portfolio">
+      <div className="portfolio-title">
+        <h2>Portfolio</h2>
+      </div>
+      <div className="portfolio-container">
+        {mywork_data.map((work, index) => {
+          return (
+            <div>
+              <div><h3>{work.w_name}</h3><p>{work.description}</p><ul>{work.languages.map((language, i) => {return <li key={i}>{language}</li>})}</ul></div>
+              <img key={index} src={work.w_img} alt="" />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
