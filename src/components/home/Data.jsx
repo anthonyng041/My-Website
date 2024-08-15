a901da515a9d205d26a80eec6dd777e1c57122a9
Anthony Nguyen <anthonyng041@gmail.com>
2024-08-14 21:49:44 -0400

    Update

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -7,9 +7,9 @@ const Data = () => {
         <h1 className="home__title">
             Anthony Nguyen
         </h1>
-        <h3 className="home__subtitle">Junior Developer</h3>
+        <h3 className="home__subtitle"> Master's Student in IT Engineering</h3>
         <br />
-        <p className="home__description">I am a recent graduate from Concordia University with a bachelor's degree in Computer Science and I am dedicated to self-improvement.</p>
+        <p className="home__description">I have recently graduated from Concordia University with a bachelor's degree in Computer Science and I am dedicated to self-improvement. I am currently pursuing a master's degree in IT Engineering at ÉTS.</p>
         <br />
         <Socials />
     </div>
